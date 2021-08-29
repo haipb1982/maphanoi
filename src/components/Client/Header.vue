5e3c3a63d5d68b790a494f48cffb969099d22e2b
haipb <phan.ba.hai@rewardstyle.com>
2021-08-29 09:15:57 +0700

    change header

--- src/components/Client/Header.vue
+++ src/components/Client/Header.vue
@@ -2,14 +2,14 @@
 	<header class="site-header">
 		<div class="site-header-container">
 			<div class="header-menu">
-				<ul v-if="urlLink">
-					<li class="">
-						<router-link tag="a" :to="urlLink.DITICH">Di Tích </router-link>
-					</li>
+				<ul v-if="urlLink">					
 					<li class="">
 						<router-link tag="a" :to="urlLink.DISAN"
 							>Di sản văn hóa
 						</router-link>
+					</li>
+					<li class="">
+						<router-link tag="a" :to="urlLink.DITICH">Di Tích </router-link>
 					</li>
 					<li class="">
 						<router-link tag="a" :to="urlLink.THAMQUAN"
