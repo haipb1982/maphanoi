d9db3ccab49dac778b1acd4b0ba1c650d999670d
haipb <phan.ba.hai@rewardstyle.com>
2021-11-22 19:06:07 +0700

    update

--- google/app.py
+++ google/app.py
@@ -9,19 +9,47 @@ def writeFile(res,file_name):
     f.write(json.dumps(res))
     f.close()
 
+def readJsonFile(filename):
+    # data = {}
+    with open(filename) as json_file:
+        data = json.load(json_file)
+    return data
+
+
 def writeData(records_data, file_name):
     jsondata = []
+    _quan = readJsonFile('quan_huyen.json')
+    _phuong = readJsonFile('phuong_xa.json')
+
     for item in records_data:
         try:
             if item['store_name'] and item['coordinates']:
                 item['id_detail'] = str(uuid.uuid4())
+                item['simistorelocator_id'] = str(uuid.uuid4())
+                item['zoom_level'] = 14
+                
                 _coordinates = item['coordinates'].split(',')
                 item['latitude'] = float(_coordinates[0])
                 item['longitude'] = float(_coordinates[1])
+                
+                phuong = item['phuong'] 
+                
+                for p in _phuong:
+                    if phuong in p['name']:
+                        item['phuong'] = str(p['id'])
+                        item['city'] = str(p['districtID'])
+
+                quan = item['city'] 
+                for q in _quan:
+                    if quan in q['name']:
+                        item['city'] = str(q['id'])
+
                 jsondata.append(item)
         except Exception as ex:
-            print('Data row FAILED: ', item )
             print('Error: ',ex)
+            print('Data row FAILED: ', item )
+            
+    
     writeFile(jsondata, file_name)
 
 
@@ -44,7 +72,8 @@ def getGoogleSheet():
         #  get all the records of the data
         records_data = sheet_instance.get_all_records()
         writeData(records_data,'disanvanhoa.json')
-    except:
+    except Exception as ex:
+        print('Error: ',ex)
         print('Create disanvanhoa.json FAILED!')
         pass
 
@@ -54,7 +83,8 @@ def getGoogleSheet():
         #  get all the records of the data
         records_data = sheet_instance.get_all_records()
         writeData(records_data,'ditich.json')
-    except:
+    except Exception as ex:
+        print('Error: ',ex)
         print('Create ditich.json FAILED!')
         pass
 
@@ -64,29 +94,30 @@ def getGoogleSheet():
         #  get all the records of the data
         records_data = sheet_instance.get_all_records()
         writeData(records_data,'thamquanao.json')
-    except:
+    except Exception as ex:
+        print('Error: ',ex)
         print('Create thamquanao.json FAILED!')
         pass
 
-    try:
-        # -> get sheet thamquanao of the Spreadsheet
-        sheet_instance = sheet.get_worksheet(3)
-        #  get all the records of the data
-        records_data = sheet_instance.get_all_records()
-        writeFile(records_data,'quan_huyen.json')
-    except:
-        print('Create quan_huyen.json FAILED!')
-        pass
+    # try:
+    #     # -> get sheet thamquanao of the Spreadsheet
+    #     sheet_instance = sheet.get_worksheet(3)
+    #     #  get all the records of the data
+    #     records_data = sheet_instance.get_all_records()
+    #     writeFile(records_data,'quan_huyen.json')
+    # except:
+    #     print('Create quan_huyen.json FAILED!')
+    #     pass
 
-    try:
-        # -> get sheet thamquanao of the Spreadsheet
-        sheet_instance = sheet.get_worksheet(4)
-        #  get all the records of the data
-        records_data = sheet_instance.get_all_records()
-        writeFile(records_data,'phuong_xa.json')
-    except:
-        print('Create phuong_xa.json FAILED!')
-        pass
+    # try:
+    #     # -> get sheet thamquanao of the Spreadsheet
+    #     sheet_instance = sheet.get_worksheet(4)
+    #     #  get all the records of the data
+    #     records_data = sheet_instance.get_all_records()
+    #     writeFile(records_data,'phuong_xa.json')
+    # except:
+    #     print('Create phuong_xa.json FAILED!')
+    #     pass
 
 
 # main
