c90fca1fa6b6d924eb75cd69d6dc673bde20737a
haipb <phan.ba.hai@rewardstyle.com>
2021-11-22 18:24:20 +0700

    update python

--- google/app.py
+++ google/app.py
@@ -1,6 +1,7 @@
-#   pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+#   pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib gspread uuid
 import gspread
 import json
+import uuid
 from oauth2client.service_account import ServiceAccountCredentials
 
 def writeFile(res,file_name):
@@ -13,13 +14,14 @@ def writeData(records_data, file_name):
     for item in records_data:
         try:
             if item['store_name'] and item['coordinates']:
+                item['id_detail'] = str(uuid.uuid4())
                 _coordinates = item['coordinates'].split(',')
-                # item['store_name'] = item['store_name']
                 item['latitude'] = float(_coordinates[0])
                 item['longitude'] = float(_coordinates[1])
                 jsondata.append(item)
-        except:
+        except Exception as ex:
             print('Data row FAILED: ', item )
+            print('Error: ',ex)
     writeFile(jsondata, file_name)
 
 
@@ -28,7 +30,7 @@ def getGoogleSheet():
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
     # add credentials to the account
-    creds = ServiceAccountCredentials.from_json_keyfile_name('google_key.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('app_google_key.json', scope)
 
     # authorize the clientsheet 
     client = gspread.authorize(creds)
@@ -58,12 +60,32 @@ def getGoogleSheet():
 
     try:
         # -> get sheet thamquanao of the Spreadsheet
-        sheet_instance = sheet.get_worksheet(1)
+        sheet_instance = sheet.get_worksheet(2)
         #  get all the records of the data
         records_data = sheet_instance.get_all_records()
         writeData(records_data,'thamquanao.json')
     except:
         print('Create thamquanao.json FAILED!')
+        pass
+
+    try:
+        # -> get sheet thamquanao of the Spreadsheet
+        sheet_instance = sheet.get_worksheet(3)
+        #  get all the records of the data
+        records_data = sheet_instance.get_all_records()
+        writeFile(records_data,'quan_huyen.json')
+    except:
+        print('Create quan_huyen.json FAILED!')
+        pass
+
+    try:
+        # -> get sheet thamquanao of the Spreadsheet
+        sheet_instance = sheet.get_worksheet(4)
+        #  get all the records of the data
+        records_data = sheet_instance.get_all_records()
+        writeFile(records_data,'phuong_xa.json')
+    except:
+        print('Create phuong_xa.json FAILED!')
         pass
 
 
